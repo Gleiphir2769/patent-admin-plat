a6e1ad5656811b8ef576e156f2139000330b9798
Jiaqi Shen <18863662628@163.com>
2022-11-24 18:05:46 +0800

    feat: add charts

---
+++ app/user-agent/service/charts/bar.go
@@ -0,0 +1,46 @@
+package charts
+
+import "strings"
+
+const barProfile = `{
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis: [
+    {
+      type: 'category',
+      data: $CATE,
+      axisTick: {
+        alignWithLabel: true
+      }
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value'
+    }
+  ],
+  series: [
+    {
+      name: 'Direct',
+      type: 'bar',
+      barWidth: '60%',
+      data: $DATA
+    }
+  ]
+}`
+
+func genBarProfile(cate []string, data []int) string {
+	cateTemp := strListTemplate(cate)
+	dataTemp := intListTemplate(data)
+	return strings.Replace(strings.Replace(barProfile, "$CATE", cateTemp, 1), "$DATA", dataTemp, 1)
+}
